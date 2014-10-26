7c9d6c6db5b42052c9910949261ddbc2e0e1bc04
devonjohnson <devon.johnson2015@gmail.com>
2014-10-26 17:43:28 -0500

    Cleaned up the ScheduleHoursActivity a lot. Added ability to switch which day's hours are being displayed. Also added Hours button to MainActivity

--- src/com/swengr2_di_dj_ly/letournapp_v1_0/ScheduleHoursActivity.java
+++ src/com/swengr2_di_dj_ly/letournapp_v1_0/ScheduleHoursActivity.java
@@ -1,26 +1,168 @@
 package com.swengr2_di_dj_ly.letournapp_v1_0;
 
-import java.util.Arrays;
 import java.util.Calendar;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ScheduleHoursActivity extends Activity {
 	
-	public static final int TOTAL_DAYS = 7;
+	public static final int[] DAYS_OF_WEEK = 
+			new int[] {
+		R.string.Day1Title,
+		R.string.Day2Title,
+		R.string.Day3Title,
+		R.string.Day4Title,
+		R.string.Day5Title,
+		R.string.Day6Title,
+		R.string.Day7Title
+	};
+	
+	public static final int[] ASC_MAIN_HOURS = 
+			new int[] {
+		R.string.Hours_ASC_Main_Day1,
+		R.string.Hours_ASC_Main_Day2,
+		R.string.Hours_ASC_Main_Day3,
+		R.string.Hours_ASC_Main_Day4,
+		R.string.Hours_ASC_Main_Day5,
+		R.string.Hours_ASC_Main_Day6,
+		R.string.Hours_ASC_Main_Day7
+	};
+	public static final int[] ASC_HIVE_HOURS = 
+			new int[] {
+		R.string.Hours_ASC_Hive_and_Coffee_Day1,
+		R.string.Hours_ASC_Hive_and_Coffee_Day2,
+		R.string.Hours_ASC_Hive_and_Coffee_Day3,
+		R.string.Hours_ASC_Hive_and_Coffee_Day4,
+		R.string.Hours_ASC_Hive_and_Coffee_Day5,
+		R.string.Hours_ASC_Hive_and_Coffee_Day6,
+		R.string.Hours_ASC_Hive_and_Coffee_Day7
+	};
+	public static final int[] ASC_COFFEE_HOURS = 
+			new int[] {
+		R.string.Hours_ASC_Hive_and_Coffee_Day1,
+		R.string.Hours_ASC_Hive_and_Coffee_Day2,
+		R.string.Hours_ASC_Hive_and_Coffee_Day3,
+		R.string.Hours_ASC_Hive_and_Coffee_Day4,
+		R.string.Hours_ASC_Hive_and_Coffee_Day5,
+		R.string.Hours_ASC_Hive_and_Coffee_Day6,
+		R.string.Hours_ASC_Hive_and_Coffee_Day7
+	};
+	public static final int[] ASC_MAIL_HOURS = 
+			new int[] {
+		R.string.Hours_ASC_Mail_Day1,
+		R.string.Hours_ASC_Mail_Day2,
+		R.string.Hours_ASC_Mail_Day3,
+		R.string.Hours_ASC_Mail_Day4,
+		R.string.Hours_ASC_Mail_Day5,
+		R.string.Hours_ASC_Mail_Day6,
+		R.string.Hours_ASC_Mail_Day7
+	};
+
+	public static final int[] SERV_LIBRARY_HOURS = 
+			new int[] {
+		R.string.Hours_Services_Library_Day1,
+		R.string.Hours_Services_Library_Day2,
+		R.string.Hours_Services_Library_Day3,
+		R.string.Hours_Services_Library_Day4,
+		R.string.Hours_Services_Library_Day5,
+		R.string.Hours_Services_Library_Day6,
+		R.string.Hours_Services_Library_Day7
+	};
+	public static final int[] SERV_REGISTRAR_HOURS =
+			new int[] {
+		R.string.Hours_Services_Registrar_Day1,
+		R.string.Hours_Services_Registrar_Day2,
+		R.string.Hours_Services_Registrar_Day3,
+		R.string.Hours_Services_Registrar_Day4,
+		R.string.Hours_Services_Registrar_Day5,
+		R.string.Hours_Services_Registrar_Day6,
+		R.string.Hours_Services_Registrar_Day7
+	};
+	public static final int[] SERV_FIN_AID_HOURS = 
+			new int[] {
+		R.string.Hours_Services_FinancialAid_Day1,
+		R.string.Hours_Services_FinancialAid_Day2,
+		R.string.Hours_Services_FinancialAid_Day3,
+		R.string.Hours_Services_FinancialAid_Day4,
+		R.string.Hours_Services_FinancialAid_Day5,
+		R.string.Hours_Services_FinancialAid_Day6,
+		R.string.Hours_Services_FinancialAid_Day7
+	};
+	public static final int[] SERV_CASHIER_HOURS = 
+			new int[] {
+		R.string.Hours_Services_Cashier_Day1,
+		R.string.Hours_Services_Cashier_Day2,
+		R.string.Hours_Services_Cashier_Day3,
+		R.string.Hours_Services_Cashier_Day4,
+		R.string.Hours_Services_Cashier_Day5,
+		R.string.Hours_Services_Cashier_Day6,
+		R.string.Hours_Services_Cashier_Day7
+	};
+
+	public static final int[] SAGA_BREAKFAST_HOURS = 
+			new int[] {
+		R.string.Hours_SAGA_Breakfast_Day1,
+		R.string.Hours_SAGA_Breakfast_Day2,
+		R.string.Hours_SAGA_Breakfast_Day3,
+		R.string.Hours_SAGA_Breakfast_Day4,
+		R.string.Hours_SAGA_Breakfast_Day5,
+		R.string.Hours_SAGA_Breakfast_Day6,
+		R.string.Hours_SAGA_Breakfast_Day7
+	};
+	public static final int[] SAGA_LUNCH_HOURS = 
+			new int[] {
+		R.string.Hours_SAGA_Lunch_Day1,
+		R.string.Hours_SAGA_Lunch_Day2,
+		R.string.Hours_SAGA_Lunch_Day3,
+		R.string.Hours_SAGA_Lunch_Day4,
+		R.string.Hours_SAGA_Lunch_Day5,
+		R.string.Hours_SAGA_Lunch_Day6,
+		R.string.Hours_SAGA_Lunch_Day7
+	};
+	public static final int[] SAGA_DINNER_HOURS = 
+			new int[] {
+		R.string.Hours_SAGA_Dinner_Day1,
+		R.string.Hours_SAGA_Dinner_Day2,
+		R.string.Hours_SAGA_Dinner_Day3,
+		R.string.Hours_SAGA_Dinner_Day4,
+		R.string.Hours_SAGA_Dinner_Day5,
+		R.string.Hours_SAGA_Dinner_Day6,
+		R.string.Hours_SAGA_Dinner_Day7
+	};
+	
+	public static final int[][] ASC_HOURS = 
+			new int[][] {
+		ASC_MAIN_HOURS,
+		ASC_HIVE_HOURS,
+		ASC_COFFEE_HOURS,
+		ASC_MAIL_HOURS
+	};
+	public static final int[][] SERV_HOURS = 
+			new int[][] {
+		SERV_LIBRARY_HOURS,
+		SERV_REGISTRAR_HOURS,
+		SERV_FIN_AID_HOURS,
+		SERV_CASHIER_HOURS
+	};
+	public static final int[][] SAGA_HOURS = 
+			new int[][] {
+		SAGA_BREAKFAST_HOURS,
+		SAGA_LUNCH_HOURS,
+		SAGA_DINNER_HOURS
+	};
 	
 	private TextView[] ascTable;
 	private TextView[] servTable;
 	private TextView[] sagaTable;
 	private TextView dayView;
-	
-	private HoursData hoursData;
+	private String[] dayStrings;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +198,9 @@ public class ScheduleHoursActivity extends Activity {
 	 */
 	private void initActivityObjects() {
 		
-		hoursData = new HoursData();
-		
-		ascTable = new TextView[hoursData.ASC_HOURS.length];
-		servTable = new TextView[hoursData.SERV_HOURS.length];
-		sagaTable = new TextView[hoursData.SAGA_HOURS.length];
+		ascTable = new TextView[ASC_HOURS.length];
+		servTable = new TextView[SERV_HOURS.length];
+		sagaTable = new TextView[SAGA_HOURS.length];
 		
 		ascTable[0] = (TextView)findViewById(R.id.Editable_ASC_Main_Hours);
 		ascTable[1] = (TextView)findViewById(R.id.Editable_ASC_Hive_Hours);
@@ -78,11 +218,36 @@ public class ScheduleHoursActivity extends Activity {
 		
 		dayView = (TextView)findViewById(R.id.DayText);
 		
-		Calendar cal = Calendar.getInstance();
-		int day = (cal.get(Calendar.DAY_OF_WEEK) - 2) % TOTAL_DAYS;
+		dayStrings = new String[DAYS_OF_WEEK.length];
+		for (int i = 0; i < DAYS_OF_WEEK.length; i++)
+			dayStrings[i] = getString(DAYS_OF_WEEK[i]);
 		
-		dayView.setText("Displaying " + hoursData.DAYS_OF_WEEK[day] +
-				" hours");
+		/*
+		 * Explanation of the modular arithmetic below:
+		 * 
+		 * Calendar.get(Calendar.DAY_OF_WEEK) returns the following
+		 * Sunday - 1
+		 * Monday - 2
+		 * Tuesday - 3
+		 * Wednesday - 4
+		 * Thursday - 5
+		 * Friday - 6
+		 * Saturday - 7
+		 * 
+		 * We need this order:
+		 * Sunday - 6
+		 * Monday - 0
+		 * Tuesday - 1
+		 * Wednesday - 2
+		 * Thursday - 3
+		 * Friday - 4
+		 * Saturday - 5
+		 * 
+		 * @author Devon Johnson
+		 */
+		Calendar cal = Calendar.getInstance();
+		int day = (cal.get(Calendar.DAY_OF_WEEK) + 5) % DAYS_OF_WEEK.length;
+		// Start display with current day of the week
 		
 		updateLabelsToDay(day);
 		
@@ -90,178 +255,57 @@ public class ScheduleHoursActivity extends Activity {
 	
 	/**
 	 * Changes all of the hours labels in the layout
-	 * to reflect the hours of a certain day.
+	 * to reflect the hours of a certain day. Also
+	 * updates the label at the bottom to reflect
+	 * which day has been selected
 	 * 
 	 * @param day 0-6 to reflect the day to display
 	 * @return true if operation is a success
 	 * @author Devon Johnson
 	 */
 	private boolean updateLabelsToDay(int day) {
-		if (day < 0 || day >= TOTAL_DAYS || hoursData == null)
+		if (day < 0 || day >= DAYS_OF_WEEK.length)
 			return false;
 		for (int i = 0; i < ascTable.length; i++)
-			ascTable[i].setText(hoursData.ASC_HOURS[i][day]);
+			ascTable[i].setText(getString(ASC_HOURS[i][day]));
 		for (int i = 0; i < servTable.length; i++)
-			servTable[i].setText(hoursData.SERV_HOURS[i][day]);
+			servTable[i].setText(getString(SERV_HOURS[i][day]));
 		for (int i = 0; i < sagaTable.length; i++)
-			sagaTable[i].setText(hoursData.SAGA_HOURS[i][day]);
+			sagaTable[i].setText(getString(SAGA_HOURS[i][day]));
+		dayView.setText("Displaying " + dayStrings[day]+ " Hours");
 		return true;
 	} // end updateLabelsToDay
 	
+	/**
+	 * Called when "Change Day" button is pressed.
+	 * Displays an alert dialog so that the user can
+	 * select a different day for which to view hours.
+	 * 
+	 * @param view Required for onClick attribute. Not used.
+	 * @author Devon Johnon
+	 */
 	public void changeDay(View view) {
-		Toast.makeText(getApplicationContext(), "Not implemented yet", Toast.LENGTH_LONG).show();
+		
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		alert.setTitle("Change Day");
+		
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {	
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		alert.setItems(dayStrings, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dayView.setText("Displaying " + dayStrings[which] + " hours");
+				updateLabelsToDay(which);
+				dialog.dismiss();
+			}
+		});
+		alert.show();
+		
 	} // end changeDay
-	
-	private class HoursData {
-		
-		public final String[] ASC_MAIN_HOURS;
-		public final String[] ASC_HIVE_HOURS;
-		public final String[] ASC_COFFEE_HOURS;
-		public final String[] ASC_MAIL_HOURS;
-
-		public final String[] SERV_LIBRARY_HOURS;
-		public final String[] SERV_REGISTRAR_HOURS;
-		public final String[] SERV_FIN_AID_HOURS;
-		public final String[] SERV_CASHIER_HOURS;
-		
-		public final String[] SAGA_BREAKFAST_HOURS;
-		public final String[] SAGA_LUNCH_HOURS;
-		public final String[] SAGA_DINNER_HOURS;
-		
-		public final String[][] ASC_HOURS;
-		public final String[][] SERV_HOURS;
-		public final String[][] SAGA_HOURS;
-		
-		public final String[] DAYS_OF_WEEK;
-		
-		public HoursData() {
-			
-			ASC_MAIN_HOURS = new String[] {
-				getString(R.string.Hours_ASC_Main_Day1),
-				getString(R.string.Hours_ASC_Main_Day2),
-				getString(R.string.Hours_ASC_Main_Day3),
-				getString(R.string.Hours_ASC_Main_Day4),
-				getString(R.string.Hours_ASC_Main_Day5),
-				getString(R.string.Hours_ASC_Main_Day6),
-				getString(R.string.Hours_ASC_Main_Day7)
-			};
-			ASC_HIVE_HOURS = new String[] {
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day1),
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day2),
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day3),
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day4),
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day5),
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day6),
-				getString(R.string.Hours_ASC_Hive_and_Coffee_Day7)
-			};
-			ASC_COFFEE_HOURS = Arrays.copyOf(
-					ASC_HIVE_HOURS, ASC_HIVE_HOURS.length);
-			ASC_MAIL_HOURS = new String[] {
-				getString(R.string.Hours_ASC_Mail_Day1),
-				getString(R.string.Hours_ASC_Mail_Day2),
-				getString(R.string.Hours_ASC_Mail_Day3),
-				getString(R.string.Hours_ASC_Mail_Day4),
-				getString(R.string.Hours_ASC_Mail_Day5),
-				getString(R.string.Hours_ASC_Mail_Day6),
-				getString(R.string.Hours_ASC_Mail_Day7)
-			};
-			
-			SERV_LIBRARY_HOURS = new String[] {
-				getString(R.string.Hours_Services_Library_Day1),
-				getString(R.string.Hours_Services_Library_Day2),
-				getString(R.string.Hours_Services_Library_Day3),
-				getString(R.string.Hours_Services_Library_Day4),
-				getString(R.string.Hours_Services_Library_Day5),
-				getString(R.string.Hours_Services_Library_Day6),
-				getString(R.string.Hours_Services_Library_Day7)
-			};
-			SERV_REGISTRAR_HOURS = new String[] {
-				getString(R.string.Hours_Services_Registrar_Day1),
-				getString(R.string.Hours_Services_Registrar_Day2),
-				getString(R.string.Hours_Services_Registrar_Day3),
-				getString(R.string.Hours_Services_Registrar_Day4),
-				getString(R.string.Hours_Services_Registrar_Day5),
-				getString(R.string.Hours_Services_Registrar_Day6),
-				getString(R.string.Hours_Services_Registrar_Day7)
-			};
-			SERV_FIN_AID_HOURS = new String[] {
-				getString(R.string.Hours_Services_FinancialAid_Day1),
-				getString(R.string.Hours_Services_FinancialAid_Day2),
-				getString(R.string.Hours_Services_FinancialAid_Day3),
-				getString(R.string.Hours_Services_FinancialAid_Day4),
-				getString(R.string.Hours_Services_FinancialAid_Day5),
-				getString(R.string.Hours_Services_FinancialAid_Day6),
-				getString(R.string.Hours_Services_FinancialAid_Day7)
-			};
-			SERV_CASHIER_HOURS = new String[] {
-				getString(R.string.Hours_Services_Cashier_Day1),
-				getString(R.string.Hours_Services_Cashier_Day2),
-				getString(R.string.Hours_Services_Cashier_Day3),
-				getString(R.string.Hours_Services_Cashier_Day4),
-				getString(R.string.Hours_Services_Cashier_Day5),
-				getString(R.string.Hours_Services_Cashier_Day6),
-				getString(R.string.Hours_Services_Cashier_Day7)
-			};
-			
-			SAGA_BREAKFAST_HOURS = new String[] {
-				getString(R.string.Hours_SAGA_Breakfast_Day1),
-				getString(R.string.Hours_SAGA_Breakfast_Day2),
-				getString(R.string.Hours_SAGA_Breakfast_Day3),
-				getString(R.string.Hours_SAGA_Breakfast_Day4),
-				getString(R.string.Hours_SAGA_Breakfast_Day5),
-				getString(R.string.Hours_SAGA_Breakfast_Day6),
-				getString(R.string.Hours_SAGA_Breakfast_Day7)
-			};
-			SAGA_LUNCH_HOURS = new String[] {
-				getString(R.string.Hours_SAGA_Lunch_Day1),
-				getString(R.string.Hours_SAGA_Lunch_Day2),
-				getString(R.string.Hours_SAGA_Lunch_Day3),
-				getString(R.string.Hours_SAGA_Lunch_Day4),
-				getString(R.string.Hours_SAGA_Lunch_Day5),
-				getString(R.string.Hours_SAGA_Lunch_Day6),
-				getString(R.string.Hours_SAGA_Lunch_Day7)
-			};
-			SAGA_DINNER_HOURS = new String[] {
-				getString(R.string.Hours_SAGA_Dinner_Day1),
-				getString(R.string.Hours_SAGA_Dinner_Day2),
-				getString(R.string.Hours_SAGA_Dinner_Day3),
-				getString(R.string.Hours_SAGA_Dinner_Day4),
-				getString(R.string.Hours_SAGA_Dinner_Day5),
-				getString(R.string.Hours_SAGA_Dinner_Day6),
-				getString(R.string.Hours_SAGA_Dinner_Day7)
-			};
-			
-			ASC_HOURS = new String[][] {
-				ASC_MAIN_HOURS,
-				ASC_HIVE_HOURS,
-				ASC_COFFEE_HOURS,
-				ASC_MAIL_HOURS
-			};
-			SERV_HOURS = new String[][] {
-				SERV_LIBRARY_HOURS,
-				SERV_REGISTRAR_HOURS,
-				SERV_FIN_AID_HOURS,
-				SERV_CASHIER_HOURS
-			};
-			SAGA_HOURS = new String[][] {
-				SAGA_BREAKFAST_HOURS,
-				SAGA_LUNCH_HOURS,
-				SAGA_DINNER_HOURS
-			};
-			
-			DAYS_OF_WEEK = new String[] {
-				getString(R.string.Day1Title),
-				getString(R.string.Day2Title),
-				getString(R.string.Day3Title),
-				getString(R.string.Day4Title),
-				getString(R.string.Day5Title),
-				getString(R.string.Day6Title),
-				getString(R.string.Day7Title)
-			};
-			
-		} // end constructor
-		
-	} // end HoursData inner class
 	
 } // end ScheduleHoursActivity class
 
