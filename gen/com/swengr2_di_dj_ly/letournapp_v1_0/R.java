a709878203c74ff86670293ca01f47c2660e1233
David Ilgenfritz <davidilgenfritz@letu.edu>
2014-10-26 22:35:49 -0500

    Add style for menu buttons

--- gen/com/swengr2_di_dj_ly/letournapp_v1_0/R.java
+++ gen/com/swengr2_di_dj_ly/letournapp_v1_0/R.java
@@ -23,7 +23,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int table_row_border=0x7f020001;
+        public static final int menu_button=0x7f020001;
+        public static final int table_row_border=0x7f020002;
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080002;
