b4893125878af754ff06509caae3a1417a0b0385
devonjohnson <devon.johnson2015@gmail.com>
2014-11-17 20:44:16 -0600

    I set up the Announcements to display titles in a table. If one of the announcements is clicked, the description is displayed.

--- src/com/swengr2_di_dj_ly/letournapp_v1_0/AnnouncementsActivity.java
+++ src/com/swengr2_di_dj_ly/letournapp_v1_0/AnnouncementsActivity.java
@@ -2,18 +2,42 @@ package com.swengr2_di_dj_ly.letournapp_v1_0;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class AnnouncementsActivity extends MenuActivity {
 
-	private TextView myTextView;
 	protected static final String ANNOUNC_URL = 
 			"http://letustartpage.blogspot.com/feeds/" +
 			"posts/default?alt=rss";
+	private String webData;
+	private TableLayout announcTable;
+	
+	public static final String[][] utf8ReplaceKeys = {
+        {"\"", "&quot;"},
+        {"&", "&amp;"},
+        {"<", "&lt;"},
+        {">", "&gt;"},
+        {" ", "&nbsp;"}
+    };
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,20 +81,27 @@ public class AnnouncementsActivity extends MenuActivity {
 			sb = new StringBuilder("Error retrieving data");
 		} // end try-catch
 		
-		setTextViewText(sb.toString());
+//		setTextViewText(sb.toString());
+		webData = decodeUtf8(sb.toString());
+//		parseData();
+		
+//		webReader = reader;
+		parseData();
+
+//		setTextViewText(webData);
 		
 	} // end onWebPageDataObtained
 
 	@Override
 	public void onExceptionRetrievingNetworkData(Exception ex) {
-		setTextViewText(ex.toString());
+//		setTextViewText(ex.toString());
 	} // end onExceptionRetrievingNetworkData
 
 	@Override
 	protected void initActivityObjects() {
 		
 		url = ANNOUNC_URL;
-		myTextView = (TextView) findViewById(R.id.textView1);
+		announcTable = (TableLayout)findViewById(R.id.announcementsTable);
 		networkChannel = new UnsecureLetuNetworkThread(url, getApplicationContext());
 		startNetworkThread();
 		
@@ -78,22 +109,222 @@ public class AnnouncementsActivity extends MenuActivity {
 
 	@Override
 	protected void parseData() {
+		if (webData == null)
+			return;
 		
-	}
+		List<RssItem> rssItems = new ArrayList<RssItem>();
+		RssItem currentItem = null;
+		
+		try {
+			
+			XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
+			parser.setInput(new StringReader(webData));
+			int eventType = parser.getEventType();
+			boolean isCreatingItem = false;
+			
+			boolean isReadingDescription = false;
+			StringBuilder descriptionStrBuilder = new StringBuilder();
+			
+			while (eventType != XmlPullParser.END_DOCUMENT) {
+				
+				try {
+					String tagName = parser.getName();
+					
+					switch (eventType) {
+					
+						case XmlPullParser.START_TAG:
+							
+							// Item tag signifies beginning of RSS Item
+							if (tagName.equalsIgnoreCase("item")) {
+	                            currentItem = new RssItem();
+	                            isCreatingItem = true;
+	                        }
+	                        
+	                        // Title tag signifies title of Announcement
+	                        else if(tagName.equalsIgnoreCase("title") && isCreatingItem) {
+	                            currentItem.setTitle(parser.nextText());
+	                        }
+	                        
+	                        // Description tag signifies description of Announcement
+	                        else if(tagName.equalsIgnoreCase("description") && isCreatingItem) {
+	                        	descriptionStrBuilder = new StringBuilder();
+	                        	isReadingDescription = true;
+	                        }
+	                        
+	                        // Link tag signifies link of Announcement
+	                        else if(tagName.equalsIgnoreCase("link") && isCreatingItem) {
+	                            currentItem.setLink(parser.nextText());
+	                        }
+							
+	                        break;
+	                        
+						case XmlPullParser.END_TAG:
+							
+							// End item tag signifies that this item is finished and can be added to the list
+							if (tagName.equalsIgnoreCase("item")) {
+	                    		rssItems.add(currentItem);
+	                    		isCreatingItem = false;
+	                    	}
+							
+							// If reading description, 
+							else if (isReadingDescription) {
+								
+								if (tagName.equalsIgnoreCase("o:p") ||
+										tagName.equalsIgnoreCase("br") ||
+										tagName.equalsIgnoreCase("li")) {
+									descriptionStrBuilder.append("\n");
+									// Skip to next line o:p or br encountered
+								}
+								
+								else if (tagName.equalsIgnoreCase("description")) {
+									
+									isReadingDescription = false;
+									currentItem.setDescription(descriptionStrBuilder.toString());
+									
+								}
+								
+							} // end if isReadingDescription
+							
+	                    	break;
+	                    
+						case XmlPullParser.TEXT:
+							
+							if (isReadingDescription) {
+								descriptionStrBuilder.append(parser.getText());
+							}
+							
+							break;
+	                    	
+	                    default:
+	                    	break;
+						
+					} // end switch
+					
+				} finally {
+					eventType = parser.next();
+				}
+				
+			} // end while
+			
+			for (RssItem rssItem : rssItems) {
+				
+				TableRow row = new TableRow(this);
+				
+				TextView tv = new TextView(this);
+	        	tv.setText(rssItem.getTitle());
+	        	tv.setBackgroundResource(R.drawable.table_row_border);
+	        	tv.setGravity(Gravity.LEFT);
+	        	tv.setPadding(15, 20, 15, 20);
+	        	tv.setTextSize(18);
+	        	
+	        	row.addView(tv);
+	        	row.setOnClickListener(new AnnouncementListener(rssItem, this));
+	        	addRowToTable(row);
+				
+			} // end for
+			
+		} catch (Exception e) { 
+//			setTextViewText(e.toString());
+		}
+		
+	} // end parseData
 	
-	private void setTextViewText(String data) {
-		
-		final TextView textView = myTextView;
-		final String theData = data;
-		
+	private void addRowToTable(TableRow row) {
+		final TableRow theRow = row;
 		runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
-				textView.setText(theData);
-			} // end run
+				announcTable.addView(theRow);
+			}
 		});
+	} // end addRowToTable
+	
+	private String decodeUtf8(String original) {
 		
-	} // end setTextViewText
+		String utf8 = original;
+		for (String[] utf8ReplaceKey : utf8ReplaceKeys)
+			utf8 = utf8.replaceAll(utf8ReplaceKey[1], utf8ReplaceKey[0]);
+		return utf8;
+	
+	} // end decodeUtf8
+	
+	public static class AnnouncementListener implements OnClickListener {
+		
+		private RssItem announcement;
+		private Context context;
+		
+		public AnnouncementListener(RssItem announcement, Context context) {
+			this.announcement = announcement;
+			this.context = context;
+		}
+		
+		@Override
+		public void onClick(View arg0) {
+			
+			AlertDialog.Builder alert = new AlertDialog.Builder(
+					context);
+			
+			ScrollView sv = new ScrollView(context);
+			
+			TextView tv = new TextView(context);
+			tv.setText(announcement.getDescription());
+			tv.setTextSize(18);
+			tv.setPadding(15, 15, 15, 15);
+			sv.addView(tv);
+			alert.setView(sv);
+			
+			alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int arg1) {
+					dialog.dismiss();
+				}
+			});
+			alert.show();
+			
+		}
+		
+	}
+	
+	public static class RssItem {
+		
+		private String title;
+		private String description;
+		private String link;
+		
+		public String getTitle() {
+			return title;
+		}
+		
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public String getDescription() {
+			return description;
+		}
+		
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		
+		public String getLink() {
+			return link;
+		}
+		
+		public void setLink(String link) {
+			this.link = link;
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("\n\nTitle: " + title);
+			sb.append("\nDescription: " + description);
+			sb.append("\nLink: " + link);
+			return sb.toString();
+		}
+		
+	} // end RssItem class
 	
 } // end AnnouncementsActivity class
 
